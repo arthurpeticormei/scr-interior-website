4b61916cb73d8a6fc57ef33fbe80d1489a344683
arthurpeticormei <arthurpeticormei@gmail.com>
2023-10-13 13:31:58 -0300

    Data attributes name changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,32 +1,41 @@
+// ##### scroll window functions trigger #####
+
+window.onscroll = () => {
+  showNavbar();
+  highlightNavbarLink();
+  fadeInEffect();
+}
+
 // ##### show navbar function #####
 
-window.onscroll = function() {showNavbar(); highlightNavbarLinks()}
+const navbarHeaderElement = document.querySelector("[data-showNavbarHeader]");
 
 function showNavbar() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.querySelector("header").style.top = "0";
+  let documentElementScrollTop = document.documentElement.scrollTop;
+  
+  if (documentElementScrollTop > 20) {
+    navbarHeaderElement.style.top = "0";
   } else {
-    document.querySelector("header").style.top = "-110px";
+    navbarHeaderElement.style.top = "-110px";
   }
 }
 
+// ##### highlight navbar link function #####
 
-// ##### scroll spy #####
+const navbarLinkSectionElements = document.querySelectorAll("[data-highlightNavbarLinkSection]");
+const navbarLinkBtnElements = document.querySelectorAll("[data-highlightNavbarLinkBtn]");
 
-let sections = document.querySelectorAll(".section");
-let navbarLinks = document.querySelectorAll(".navbar-link");
-
-function highlightNavbarLinks() {
-  sections.forEach(section => {
+function highlightNavbarLink() {
+  navbarLinkSectionElements.forEach(navbarLinkSectionElement => {
     let top = window.scrollY;
-    let offset = section.offsetTop - 150;
-    let height = section.offsetHeight;
-    let id = section.getAttribute("id");
+    let offset = navbarLinkSectionElement.offsetTop - 150;
+    let height = navbarLinkSectionElement.offsetHeight;
+    let id = navbarLinkSectionElement.getAttribute("id");
 
     if(top >= offset && top < offset + height) {
-      navbarLinks.forEach(navbarLink => {
-        navbarLink.classList.remove("active");
-        document.querySelector(".navbar-link[href*=" + id + "]").classList.add("active");
+      navbarLinkBtnElements.forEach(navbarLinkBtnElement => {
+        navbarLinkBtnElement.classList.remove("active");
+        document.querySelector("[data-highlightNavbarLinkBtn][href*=" + id + "]").classList.add("active");
       });
 
     }
@@ -34,34 +43,76 @@ function highlightNavbarLinks() {
 }
 
 
+// ##### fade in effect direction #####
 
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      let fadeInElements = document.querySelectorAll("[data-fadeInDirection]");
-      fadeInElements.forEach(fadeInElement => {
-        let direction = fadeInElement.dataset.fadeindirection;
-        console.log(fadeInElement);
-        switch (direction) {
-          case 'top':
-            fadeInElement.classList.add("fadeInTop");
-            break;
-          case 'bottom':
-            fadeInElement.classList.add("fadeInBottom");
-            break;
-          case 'left':
-            fadeInElement.classList.add("fadeInLeft");
-            break;
-          case 'right':
-            fadeInElement.classList.add("fadeInRight");
-          break;
-        }
-      });
-    };
-  });
-});
+// const fadeInContainerElements = document.querySelectorAll("[data-fadeInEffectContainer]");
+// const fadeInElements = document.querySelectorAll("[data-fadeInEffectDirection]");
 
-const fadeInElementsContainers = document.querySelectorAll("[data-fadeIn]");
-fadeInElementsContainers.forEach(fadeInElementsContainer => {
-  observer.observe(fadeInElementsContainer);
-})
+// function fadeInEffect() {
+
+
+//   fadeInContainerElements.forEach(fadeInContainerElement => {
+
+//     let top = window.scrollY;
+//     let offset = fadeInContainerElement.offsetTop;
+//     let id = fadeInElement.dataset.fadeineffectid;
+
+//     let matchId = document.querySelector("[data-fadeInEffectDirection][href*=" + id + "]")
+    
+//     if(top >= offset) {
+//       fadeInElements.forEach(fadeInElement => {
+        
+//         let fadeInDirection = fadeInElement.dataset.fadeineffectdirection;
+
+//         switch (fadeInDirection) {
+//           case 'top':
+//             fadeInElement.classList.add("fadeInTop");
+//             break;
+//           case 'bottom':
+//             fadeInElement.classList.add("fadeInBottom");
+//             break;
+//           case 'left':
+//             fadeInElement.classList.add("fadeInLeft");
+//             break;
+//           case 'right':
+//             fadeInElement.classList.add("fadeInRight");
+//           break;
+//         }
+//       });
+//     }
+//   });
+// }
+
+
+
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       let fadeInElements = document.querySelectorAll("[data-fadeInDirection]");
+//       fadeInElements.forEach(fadeInElement => {
+//         let direction = fadeInElement.dataset.fadeindirection;
+//         console.log(fadeInElement);
+//         switch (direction) {
+//           case 'top':
+//             fadeInElement.classList.add("fadeInTop");
+//             break;
+//           case 'bottom':
+//             fadeInElement.classList.add("fadeInBottom");
+//             break;
+//           case 'left':
+//             fadeInElement.classList.add("fadeInLeft");
+//             break;
+//           case 'right':
+//             fadeInElement.classList.add("fadeInRight");
+//           break;
+//         }
+//       });
+//     };
+//   });
+// });
+
+// const fadeInElementsContainers = document.querySelectorAll("[data-fadeIn]");
+// fadeInElementsContainers.forEach(fadeInElementsContainer => {
+//   observer.observe(fadeInElementsContainer);
+// })
+
